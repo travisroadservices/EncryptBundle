5cc1fb451948a3baba4fd54c5214afb8bcb94403
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2023-08-11 15:00:18 +0200

    Fixed annotation/attribute issue

--- Annotations/Encrypted.php
+++ Annotations/Encrypted.php
@@ -1,21 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Mark Ogilvie
- * Date: 21/10/17
- * Time: 18:22
- */
 
 namespace SpecShaper\EncryptBundle\Annotations;
 
+use Attribute;
 use Doctrine\Common\Annotations\Annotation;
 
 /**
  * @Annotation
  * @Target("ALL")
  */
-final class Encrypted extends Annotation
+#[\Attribute(Attribute::TARGET_PROPERTY)]
+final class Encrypted
 {
-
 }
-
