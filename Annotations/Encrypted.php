8f97ed398de3d27912e8a3a25bf582c13bf8435b
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2023-08-11 15:00:18 +0200

    Fixed annotation/attribute issue

--- Annotations/Encrypted.php
+++ Annotations/Encrypted.php
@@ -8,14 +8,15 @@
 
 namespace SpecShaper\EncryptBundle\Annotations;
 
+use Attribute;
 use Doctrine\Common\Annotations\Annotation;
 
 /**
  * @Annotation
  * @Target("ALL")
  */
+#[\Attribute(Attribute::TARGET_PROPERTY)]
 final class Encrypted extends Annotation
 {
 
 }
-
